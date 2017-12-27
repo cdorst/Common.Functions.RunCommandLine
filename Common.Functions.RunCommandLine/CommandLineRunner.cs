fc1943112436323c4fd42c1f39e601185abf9c73
Christopher Dorst <chris@dorst.email>
2017-12-27 15:00:46 -0600

    Adding CommandLineRunner

---
+++ Common.Functions.RunCommandLine/CommandLineRunner.cs
@@ -0,0 +1,15 @@
+﻿using static Common.Functions.JoinCommandLineArguments.CommandLineArgumentsJoiner;
+using static Common.Functions.PromptCommandInGivenDirectory.CommandInGivenDirectoryPrompter;
+
+namespace Common.Functions.RunCommandLine
+{
+    public static class CommandLineRunner
+    {
+        public static void Run(string workingDirectory, string command, params string[] arguments)
+        {
+            var cmd = PromptIn(workingDirectory);
+            cmd.StandardInput.WriteLine(Join(command, arguments));
+            cmd.WaitForExit();
+        }
+    }
+}
